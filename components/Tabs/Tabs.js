89911c0e37a0c13c53b2c4706fc07818298ed5c0
Jacqueline <jacquelineomollo@gmail.com>
2019-06-13 21:12:47 -0400

    Added some changes to Tabs,js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,7 +8,7 @@ class TabLink {
 
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(
-      `.tab-link[data-tab="${this.tabNumber}]`
+      `.tab-link[data-tab="${this.data}]`
     );
 
     // Using the Item element, create a new instance of the TabItem class
@@ -24,13 +24,16 @@ class TabLink {
   select() {
     // Get all of the elements with the tabs-link class
 
-    const links = document.querySelectorAll(".tabs-link");
+    const links = document.getElementsByClassName(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach();
+    Array.from(links).forEach(links, function(el) {
+      el.classList.remove("tabs-link-selected");
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    //this.element=
+
+    this.element.classList.add("tabs-link-selected");
 
     // Call the select method on the item associated with this link
   }
@@ -50,7 +53,7 @@ class TabItem {
 
     // Remove the class "tabs-item-selected" from each element
     // Add a class named "tabs-item-selected" to this element
-    this.element.classList.toggle("tab-item-selected");
+    this.element.classList.toggle("links");
   }
 }
 
